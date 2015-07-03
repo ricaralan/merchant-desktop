92c99a126052ef391457878789ad7b2f6ae25005
Alan <richard.alan.oli@gmail.com>
2015-07-03 06:33:03 -0500

    added MerchantComboSQL

--- src/com/merchant/views/configuration/LineasPanel.java
+++ src/com/merchant/views/configuration/LineasPanel.java
@@ -5,6 +5,8 @@
  */
 package com.merchant.views.configuration;
 
+import com.merchant.database.models.RegimenModel;
+import com.merchant.views.MerchantComboSQL;
 import java.sql.Connection;
 import javax.swing.JTable;
 
@@ -17,12 +19,14 @@ public class LineasPanel extends javax.swing.JPanel {
     /**
      * Creates new form LineasPanel
      */
+    Connection connection;
     public LineasPanel() {
         initComponents();
     }
     
     public LineasPanel(Connection connection, JTable table) {
         initComponents();
+        this.connection = connection;
     }
 
     /**
@@ -35,8 +39,19 @@ public class LineasPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        comboPrueba = new javax.swing.JComboBox();
+        jButton1 = new javax.swing.JButton();
 
         jLabel1.setText("Lineas panel");
+
+        comboPrueba.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jButton1.setText("setModel");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -46,18 +61,34 @@ public class LineasPanel extends javax.swing.JPanel {
                 .addGap(149, 149, 149)
                 .addComponent(jLabel1)
                 .addContainerGap(160, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(comboPrueba, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(128, 128, 128)
+                .addGap(59, 59, 59)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(comboPrueba, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(44, 44, 44)
                 .addComponent(jLabel1)
                 .addContainerGap(157, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        comboPrueba.setModel(new MerchantComboSQL(connection, new RegimenModel(), "descripcionRegimenFiscal"));
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox comboPrueba;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
